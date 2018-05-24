032a6e12d90a73f7ec090e8ef71bb4cf4ea6a7fd
Gustavo Murayama <gmurayama@outlook.com>
2018-05-24 11:40:21 -0300

    Select item via keyboard

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,6 +1,53 @@
 var AutoComplete = (function () {
     var AutoComplete = function (htmlElement) {
         this.element = htmlElement;
+        this.stopListSuggestionRetrieve = false;
+
+        var objectContext = this;
+
+        this.element.addEventListener('keyup', function (event) {
+            var key = event.key | event.keyCode;
+            var itemList = event.target.parentNode.querySelectorAll('.autocomplete.dropdown ul li');
+            var itemSelected = event.target.parentNode.querySelector('.autocomplete.dropdown ul li.selected');
+            
+            var findIndex = (function (array, item) {
+                for (var i = 0; i < array.length; i++) {
+                    if (array[i] === item) {
+                        return i;
+                    }
+                }
+
+                return -1;
+            });
+
+            var addSelectedClassToItem = (function (itemList, indexOfCurrentSelectedElement, indexOfElementToSelect) {
+                var currentItem = itemList[indexOfCurrentSelectedElement];
+                var itemToBeSelected = itemList[indexOfElementToSelect];
+
+                if (currentItem)
+                    currentItem.className = currentItem.className.replace('selected', '');
+
+                if (itemToBeSelected)
+                    itemToBeSelected.className += ' selected';
+            });
+
+            if (isArrowDown(key)) {
+                var index = findIndex(itemList, itemSelected);
+                addSelectedClassToItem(itemList, index, index + 1);
+                objectContext.stopListSuggestionRetrieve = true;
+            }
+            else
+                if (isArrowUp(key)) {
+                    var index = findIndex(itemList, itemSelected);
+                    addSelectedClassToItem(itemList, index, index - 1);
+                    objectContext.stopListSuggestionRetrieve = true;
+                }
+                else
+                    if (isEnter(key)) {
+                        itemSelected.click();
+                        objectContext.stopListSuggestionRetrieve = true;
+                    }
+        });
     }
 
     AutoComplete.prototype = {
@@ -45,6 +92,11 @@ var AutoComplete = (function () {
         },
 
         getSuggestionList: function () {
+            if (this.stopListSuggestionRetrieve) {
+                this.stopListSuggestionRetrieve = false;
+                return;
+            }
+
             var config = this.loadConfiguration();
 
             if (config.text.length >= config.minLength) {
@@ -117,7 +169,7 @@ var AutoComplete = (function () {
                 var startIndex = item.toLowerCase().indexOf(textoToHighlight.toLowerCase());
                 var endIndex = startIndex + textoToHighlight.length;
                 var itemHighlight = item.slice(0, startIndex) + '<b>' + item.slice(startIndex, endIndex) + '</b>' + item.slice(endIndex, item.length);
-                dropdownList += '<li data-value="' + item + '" tabindex="1">' + itemHighlight + '</li>';
+                dropdownList += '<li class="" data-value="' + item + '" tabindex="1">' + itemHighlight + '</li>';
             }
 
             dropDownElement.querySelector('ul').innerHTML += dropdownList;
@@ -130,6 +182,35 @@ var AutoComplete = (function () {
                 listItems[i].onclick = (function () {
                     var text = this.dataset['value'];
                     element.value = text;
+                });
+
+                listItems[i].onmouseover = (function (evt) {
+                    var item = evt.target;
+
+                    var selectedItems = document.querySelectorAll('.autocomplete.dropdown ul li.selected');
+
+                    if (selectedItems != null && selectedItems != undefined) {
+                        for (var i = 0; i < selectedItems.length; i++) {
+                            var selectedItem = selectedItems[i];
+                            selectedItem.className = selectedItem.className.replace('selected', '');
+                        }
+                    }
+
+                    if (item.tagName.toLowerCase() == 'li')
+                        item.className += ' selected';
+                    else
+                        item.parentNode.className += ' selected';
+                });
+
+                listItems[i].onmouseout = (function () {
+                    var selectedItems = document.querySelectorAll('.autocomplete.dropdown ul li.selected');
+
+                    if (selectedItems != null && selectedItems != undefined) {
+                        for (var i = 0; i < selectedItems.length; i++) {
+                            var selectedItem = selectedItems[i];
+                            selectedItem.className = selectedItem.className.replace('selected', '');
+                        }
+                    }
                 });
             }
         },
@@ -147,6 +228,18 @@ var AutoComplete = (function () {
         return variable === null || variable === undefined || variable == '';
     }
     
+    function isArrowDown(key) {
+        return key === 'ArrowDown' || key === 40;
+    }
+
+    function isArrowUp(key) {
+        return key === 'ArrowUp' || key === 38;
+    }
+
+    function isEnter(key) {
+        return key === 'Enter' || key === 13;
+    }
+
     function createHttpRequest (
         url,
         method,
